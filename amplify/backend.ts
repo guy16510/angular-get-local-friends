06388d5e23698b251cb1f9aef42981da678b6226
Chris Burns <admin@chrisMacbook.local>
2025-02-18 10:25:33 -0500

    new resource

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,14 +1,38 @@
 import { defineBackend } from '@aws-amplify/backend';
+import { PolicyStatement } from 'aws-cdk-lib/aws-iam';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
 import { sayHello } from './functions/say-hello/resource';
 import { findNearbyUsers } from './functions/find-nearby-users/resource';
 import { mutateUserProfile } from './functions/mutate-user-profile/resource';
 
-defineBackend({
+const backend = defineBackend({
   auth,
   data,
   sayHello,
   findNearbyUsers,
   mutateUserProfile,
 });
+
+// Grant permissions to mutateUserProfile function
+backend.mutateUserProfile.resources.lambda.addToRolePolicy(
+  new PolicyStatement({
+    actions: [
+      'dynamodb:PutItem',
+      'dynamodb:UpdateItem',
+      'dynamodb:DeleteItem',
+      'dynamodb:GetItem',
+    ],
+    resources: ['arn:aws:dynamodb:*:*:table/UserProfileTable'],
+  })
+);
+
+// Grant read-only permissions to findNearbyUsers function
+backend.findNearbyUsers.resources.lambda.addToRolePolicy(
+  new PolicyStatement({
+    actions: ['dynamodb:GetItem', 'dynamodb:Scan', 'dynamodb:Query'],
+    resources: ['arn:aws:dynamodb:*:*:table/UserProfileTable'],
+  })
+);
+
+export default backend;
