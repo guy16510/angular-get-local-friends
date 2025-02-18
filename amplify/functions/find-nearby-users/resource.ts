39da8468e45c261748b2f6c75f13c128d60bf47a
Chris Burns <admin@chrisMacbook.local>
2025-02-18 09:58:59 -0500

    update pollicy

--- amplify/functions/find-nearby-users/resource.ts
+++ amplify/functions/find-nearby-users/resource.ts
@@ -3,7 +3,4 @@ import { defineFunction } from '@aws-amplify/backend';
 export const findNearbyUsers = defineFunction({
   name: 'find-nearby-users',
   entry: './handler.ts',
-  environment: {
-    'USER_PROFILE_TABLE' : 'UserProfileTable'
-  },
 });
