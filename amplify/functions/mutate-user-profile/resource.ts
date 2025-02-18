1a7fe2f7a4d7c3e9bb8ff31dfa3ad960d88bd809
Chris Burns <admin@chrisMacbook.local>
2025-02-18 15:00:40 -0500

    updat

--- amplify/functions/mutate-user-profile/resource.ts
+++ amplify/functions/mutate-user-profile/resource.ts
@@ -3,4 +3,7 @@ import { defineFunction } from '@aws-amplify/backend';
 export const mutateUserProfile = defineFunction({
   name: 'mutate-user-profile',
   entry: './handler.ts',
+  environment:{
+    USER_PROFILE_TABLE_NAME: `UserProfile-${process.env['AMPLIFY_PROJECT_ID']}-${process.env['AMPLIFY_ENV']}-NONE`,
+  }
 });
