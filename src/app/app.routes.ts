03421fcd6d2ba53b15c79b4cc31a64e6bb45c8b3
Chris Burns <admin@chrisMacbook.local>
2025-02-17 22:09:34 -0500

    more pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,8 @@ import { authGuard } from './guards/auth.guard';
 import { HomeComponent } from './components/home/home.component';
 import { ErrorComponent } from './components/error/error.component';
 import { LoginComponent } from './components/login/login.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { SearchComponent } from './components/search/search.component';
 
 export const routes: Routes = [
     {
@@ -27,6 +29,16 @@ export const routes: Routes = [
     {
         path: 'login',
         component: LoginComponent
+    },
+    {
+        path: 'sign-up',
+        component: SignUpComponent,
+        canActivate: [authGuard]
+    },
+    {
+        path: 'search',
+        component: SearchComponent,
+        canActivate: [authGuard]
     },
     {
         path: '**',
