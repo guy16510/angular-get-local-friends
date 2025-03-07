3424c8f315be33bdad5b72c158be7530fdf15892
Chris Burns <admin@chrisMacbook.local>
2025-03-06 21:19:26 -0500

    update

--- amplify/auth/post-confirmation/handler.ts
+++ amplify/auth/post-confirmation/handler.ts
@@ -5,23 +5,11 @@ import {
 } from '@aws-sdk/client-cognito-identity-provider';
 
 const client = new CognitoIdentityProviderClient();
-const GROUP_NAME = `EVERYONE-${process.env['AWS_BRANCH']}`;  // Use AWS_BRANCH
 
-
-// add user to group
 export const handler: PostConfirmationTriggerHandler = async (event) => {
-    console.log("Lambda environment variables:", process.env);
-  
-    const userPoolId = event.userPoolId;
-    console.log("Using User Pool ID:", userPoolId);
-  
-    const groupName = process.env['GROUP_NAME'];
-    console.log("Trying to add user to group:", groupName);
-    
-    console.log("PROCESS ENV: " + GROUP_NAME)
 
   const command = new AdminAddUserToGroupCommand({
-    GroupName: groupName,
+    GroupName: process.env['GROUP_NAME'],
     Username: event.userName,
     UserPoolId: event.userPoolId
   });
