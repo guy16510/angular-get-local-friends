d715322bd39a7842dce6dd3da385e2d4aca3c7a7
Chris Burns <admin@chrisMacbook.local>
2025-03-26 09:56:04 -0400

    removing facebook auth

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,36 +1,39 @@
 import { defineAuth, secret } from '@aws-amplify/backend';
 import { postConfirmation } from "./post-confirmation/resource";
 
+
+/**
+ * Facebook auth requires a business license, that I don't have
+ * Maybe investigate in the future.
+ */
 export const auth = defineAuth({
   loginWith: {
     email: true,
-    externalProviders: {
-      facebook: {
-        clientId: secret('FACEBOOK_APP_ID'),
-        clientSecret: secret('FACEBOOK_APP_SECRET'),
-        scopes: ['public_profile', 'email'],
-        attributeMapping: {
-          email: 'email',
-          nickname: 'name'
-        }
-      },
-      callbackUrls: [
-        'http://localhost:4200/myProfile',
-        'http://192.168.2.15:4200/myProfile',
-        'https://dev.getlocalfriends.com/myProfile',
-        'https://getlocalfriends.com/myProfile',
-        'https://getlocalfriends-auth.auth.us-east-1.amazoncognito.com/oauth2/idpresponse',
-        'https://dev-getlocalfriends-auth.auth.us-east-1.amazoncognito.com/oauth2/idpresponse'
-      ],
-      logoutUrls: [
-        'http://localhost:4200/',
-        'http://192.168.2.15:4200/',
-        'https://dev.getlocalfriends.com/',
-        'https://getlocalfriends.com/',
-        'https://getlocalfriends-auth.auth.us-east-1.amazoncognito.com/logout',
-        'https://dev-getlocalfriends-auth.auth.us-east-1.amazoncognito.com/logout',
-      ],
-    },
+    // externalProviders: {
+    //   facebook: {
+    //     clientId: secret('FACEBOOK_APP_ID'),
+    //     clientSecret: secret('FACEBOOK_APP_SECRET'),
+    //     scopes: ['public_profile', 'email'],
+    //     attributeMapping: {
+    //       email: 'email',
+    //       nickname: 'name'
+    //     }
+    //   },
+    //   callbackUrls: [
+    //     'http://localhost:4200/myProfile',
+    //     'https://dev.getlocalfriends.com/myProfile',
+    //     'https://getlocalfriends.com/myProfile',
+    //     'https://getlocalfriends-auth.auth.us-east-1.amazoncognito.com/oauth2/idpresponse',
+    //     'https://dev-getlocalfriends-auth.auth.us-east-1.amazoncognito.com/oauth2/idpresponse'
+    //   ],
+    //   logoutUrls: [
+    //     'http://localhost:4200/',
+    //     'https://dev.getlocalfriends.com/',
+    //     'https://getlocalfriends.com/',
+    //     'https://getlocalfriends-auth.auth.us-east-1.amazoncognito.com/logout',
+    //     'https://dev-getlocalfriends-auth.auth.us-east-1.amazoncognito.com/logout',
+    //   ],
+    // },
   },
   groups: [
     `EVERYONE-${process.env['AWS_BRANCH']}`,
